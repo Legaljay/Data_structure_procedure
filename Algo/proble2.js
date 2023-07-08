2e56886f99fe88fc4b8653a667df0fdb60dbfb4c
Legaljay <adesinaayobami67@gmail.com>
2023-07-08 17:38:10 +0100

    updated some file

--- Algo/proble2.js
+++ Algo/proble2.js
@@ -3,30 +3,45 @@
 /*
 
 PROCEDURE dotProduct(v1, v2)
-    ps = 0
-    FOR i = 0 TO LENGTH(v1) - 1 DO
-        ps = ps + (v1[i] * v2[i])
-    END FOR
-    RETURN ps
-END PROCEDURE
+ Algorithm: dot_product(v1, v2)
+    Initialize variable ps to 0
+    For i from 0 to length of v1 - 1
+        Multiply the ith element of v1 with the ith element of v2 and add the result to ps
+    End For
+    Return ps
+End Algorithm
 
-FUNCTION checkOrthogonality(vectors)
-    orthogonalPairs = EMPTY ARRAY
-    FOR i = 0 TO LENGTH(vectors) - 2 DO
-        FOR j = i + 1 TO LENGTH(vectors) - 1 DO
-            ps = dotProduct(vectors[i], vectors[j])
-            IF ps = 0 THEN
-                ADD [i, j] TO orthogonalPairs
-            END IF
-        END FOR
-    END FOR
-    RETURN orthogonalPairs
-END FUNCTION
+Algorithm: CheckOrthogonality(vectors)
+    For each pair of vectors (v1, v2) in vectors
+        Calculate dot product using dot_product procedure:
+        Set dp = dot_product(v1, v2)
+        If dp is equal to 0
+            Print "Vectors v1 and v2 are orthogonal"
+        Else
+            Print "Vectors v1 and v2 are not orthogonal"
+        End If
+    End For
+End Algorithm
 
-// Example usage
-vectors = [[1, 0, 2], [0, 2, 0], [3, 1, 4], [2, 2, -2]]
-orthogonalPairs = checkOrthogonality(vectors)
-OUTPUT orthogonalPairs  // Output: [[0, 1]]
+Algorithm: CheckOrthogonalityWithFunction(vectors)
+    Function: dot_product(v1, v2)
+        Initialize variable ps to 0
+        For i from 0 to length of v1 - 1
+            Multiply the ith element of v1 with the ith element of v2 and add the result to ps
+        End For
+        Return ps
+    End Function
+
+    For each pair of vectors (v1, v2) in vectors
+        Calculate dot product using dot_product function:
+        Set dp = dot_product(v1, v2)
+        If dp is equal to 0
+            Print "Vectors v1 and v2 are orthogonal"
+        Else
+            Print "Vectors v1 and v2 are not orthogonal"
+        End If
+    End For
+End Algorithm
 
 
 
@@ -34,32 +49,64 @@ OUTPUT orthogonalPairs  // Output: [[0, 1]]
 
 //implementation
 
-// Procedure: dotProduct calculates the dot product of two vectors v1 and v2
-function dotProduct(v1, v2) {
-    let ps = 0; // Variable to store the dot product
+// Procedure: dot_product calculates the dot product of two vectors v1 and v2
+function dot_product(v1, v2) {
+    let ps = 0;
     for (let i = 0; i < v1.length; i++) {
-      ps += v1[i] * v2[i]; // Accumulate the product of corresponding elements
+      ps += v1[i] * v2[i];
     }
-    return ps; // Return the dot product
+    return ps;
   }
   
-  // Function: checkOrthogonality checks for orthogonality among the given set of vectors
-  function checkOrthogonality(vectors) {
-    let orthogonalPairs = []; // Array to store pairs of orthogonal vectors
+  // Algorithm: CheckOrthogonality
+  function CheckOrthogonality(vectors) {
     for (let i = 0; i < vectors.length - 1; i++) {
       for (let j = i + 1; j < vectors.length; j++) {
-        let ps = dotProduct(vectors[i], vectors[j]); // Call the dotProduct procedure and store the result
-        if (ps === 0) {
-          orthogonalPairs.push([i, j]); // Add the indices of orthogonal vectors to orthogonalPairs
+        let dp = dot_product(vectors[i], vectors[j]);
+        if (dp === 0) {
+          console.log("Vectors v" + i + " and v" + j + " are orthogonal");
+        } else {
+          console.log("Vectors v" + i + " and v" + j + " are not orthogonal");
         }
       }
     }
-    return orthogonalPairs; // Return the array of orthogonal pairs
+  }
+  
+  // Algorithm: CheckOrthogonalityWithFunction
+  function CheckOrthogonalityWithFunction(vectors) {
+    // Function: dot_product
+    function dot_product(v1, v2) {
+      let ps = 0;
+      for (let i = 0; i < v1.length; i++) {
+        ps += v1[i] * v2[i];
+      }
+      return ps;
+    }
+  
+    for (let i = 0; i < vectors.length - 1; i++) {
+      for (let j = i + 1; j < vectors.length; j++) {
+        let dp = dot_product(vectors[i], vectors[j]);
+        if (dp === 0) {
+          console.log("Vectors v" + i + " and v" + j + " are orthogonal");
+        } else {
+          console.log("Vectors v" + i + " and v" + j + " are not orthogonal");
+        }
+      }
+    }
   }
   
   // Example usage
-  let vectors = [[1, 0, 2], [0, 2, 0], [3, 1, 4], [2, 2, -2]];
-  let orthogonalPairs = checkOrthogonality(vectors);
-  console.log(orthogonalPairs); 
+  let vectors = [
+    [1, 0, 2],
+    [0, 2, 0],
+    [3, 1, 4],
+    [2, 2, -2]
+  ];
+  
+  console.log("Using CheckOrthogonality:");
+  CheckOrthogonality(vectors);
+  
+  console.log("Using CheckOrthogonalityWithFunction:");
+  CheckOrthogonalityWithFunction(vectors);
   
   
